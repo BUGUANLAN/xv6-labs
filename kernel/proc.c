902f32eaffc85d5dafe5811959bb6c1c9567fd20
Frans Kaashoek <kaashoek@mit.edu>
2020-09-05 17:33:13 -0400

    Add sysinfo.h with struct sysinfo

--- kernel/proc.c
+++ kernel/proc.c
@@ -693,3 +693,15 @@ procdump(void)
     printf("\n");
   }
 }
+
+void
+kama_procnum(uint64 *dst)
+{
+  struct proc *p;
+  *dst = 0;
+
+  for(p = proc; p < &proc[NPROC]; p++) {
+    if(p->state != UNUSED)
+      (*dst)++;
+  }
+}
